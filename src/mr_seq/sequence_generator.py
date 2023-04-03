1e52b543e28bd5792892d3af1b89888ed37e5968
Christophe Chênes <christophe.chenes@gmail.com>
2023-04-03 13:41:30 +0200

    add sequence as plugins

--- src/mr_seq/sequence_generator.py
+++ src/mr_seq/sequence_generator.py
@@ -27,11 +27,10 @@ class SequenceGenerator:
         return v
 
     def get_sequences(self):
-        return self.available_sequences.SEQUENCE_TYPES #return seqs.SEQUENCE_TYPES
+        return self.available_sequences.SEQUENCE_TYPES
 
-    @classmethod
     def generate(
-        cls,
+        self,
         sequence_type: str,
         matrix_shape: tuple,
         title: str,
@@ -40,6 +39,36 @@ class SequenceGenerator:
         timestep: int = -1,
         verbal: bool = False,
     ):
+        # Check seq type available
+        if sequence_type not in self.available_sequences.SEQUENCE_TYPES:
+            print(
+                f"ERROR: asked sequence type ('{sequence_type}') is not available, available sequences are: {self.available_sequences.SEQUENCE_TYPES}"
+            )
+            return False
+
+        # Get the seq type generator
+        sequence_generator = self.available_sequences.SEQUENCE_GENERATORS[
+            self.available_sequences.SEQUENCE_TYPES.index(sequence_type)
+        ]
+        print(sequence_generator)
+
+        # Get the seq type class
+        SequenceClass = self.available_sequences.SEQUENCE_CLASSES[
+            self.available_sequences.SEQUENCE_TYPES.index(sequence_type)
+        ]
+        sc = SequenceClass()
+        print(sc.check())
+
+        # Generate the sequence using the parameters and the loaded SequenceClass
+        sc.generate(
+            matrix_shape,
+            title,
+            outdir,
+            physics_configuration_filename,
+            timestep,
+            verbal,
+        )
+
         return True
         # return seqs.generate_sequence(
         #     sequence_type,
--- src/mr_seq/sequences/sequences.py
+++ src/mr_seq/sequences/sequences.py
@@ -1,39 +1,88 @@
+import os.path
 from pathlib import Path
 import yaml
 
 from mr_seq.configuration import Configuration
 
 
+def importName(modulename, name):
+    """Import a named object from a module in the context of this function."""
+    try:
+        module = __import__(modulename, globals(), locals(), [name])
+    except ImportError:
+        return None
+    return vars(module)[name]
+
+
 class Sequences(Configuration):
     __default_sequences_filename = "configurations/sequences.yml"
 
-    SEQUENCE_TYPES = None
-    SEQUENCE_GENERATORS = None
+    SEQUENCE_TYPES = []
+    SEQUENCE_GENERATORS = []
+    SEQUENCE_FILES = []
+    SEQUENCE_CLASSES = []
+    SEQUENCE_PREFIX = "generate_"
+    SEQUENCE_SUFFIX = "_sequence"
+    SEQUENCE_CLASS = "Sequence_"
 
     def check_configuration(self) -> bool:
         print("Check seq config")
+
+        # Check all required keys are in the file
+        if any([key not in self.configuration for key in self.configuration_keys]):
+            print(
+                f"Configuration check fail: sequences config must contain all keys: '{self.configuration_keys}'"
+            )
+            return False
+
+        # Check sequences directory exists
+        seq_dir = Path(self.configuration["sequences_directory"])
+        if seq_dir.is_dir():
+            print("Directory of sequences exists")
+
+        # Get the names of all sequences and check that the corresponding generator file exists
+        for seq in self.configuration["sequences"]:
+            seq_name = seq["name"]
+            seq_generator = f"{self.SEQUENCE_PREFIX}{seq['name']}"
+            seq_module = f"{seq['name']}{self.SEQUENCE_SUFFIX}"
+            seq_filename = f"{seq_module}.py"
+            seq_class = f"{self.SEQUENCE_CLASS}{seq_name}"
+            # print(f"seq: {seq_name}; with generator: {seq_generator}")
+            seq_generator_path = seq_dir / seq_filename
+            if not seq_generator_path.is_file():
+                print(
+                    f"Configuration check fail: sequence '{seq_name}' generator ('{seq_generator_path}') not found."
+                )
+                continue  # Skip this sequences => this allows for incomplete seq config
+
+            # Add the sequence and its generator to the available sequences
+            self.SEQUENCE_TYPES.append(seq_name)
+            self.SEQUENCE_GENERATORS.append(seq_generator)
+            self.SEQUENCE_FILES.append(seq_filename)
+
+            # Import the generator function from the sequence file
+            module_name = "mr_seq.sequences." + seq_filename
+            # importName(f"mr_seq.sequences.{seq_module}", seq_generator)
+            SequenceClass = importName(f"mr_seq.sequences.{seq_module}", seq_class)
+            self.SEQUENCE_CLASSES.append(SequenceClass)
+
+            # Check the import worked
+            sc = SequenceClass()
+            print(sc.check())
+
         return True
 
     def __init__(self, configuration_filename: str = ""):
+        # init keys
+        self.configuration_keys = [
+            "title",
+            "sequences_directory",
+            "sequences",
+        ]
+
         # read the configuration file containing the available sequences
 
         self.configuration_filename: str = configuration_filename
         if configuration_filename == "":
             self.configuration_filename = self.__default_sequences_filename
         self.read_configuration()
-
-        '''
-        seq_configuration_path = Path(configuration_filename)
-        if seq_configuration_path.is_file():
-            with open(seq_configuration_path, "r") as seq_configuration_file:
-                try:
-                    seq_configuration = yaml.safe_load(seq_configuration_file)
-                    # TODO read sequences from configuration file
-                except yaml.YAMLError as exc:
-                    raise Exception(
-                        "Error reading the sequences configuration file: "
-                        + f"{seq_configuration_file}"
-                    ) from exc
-        else:
-            raise Exception(f"Error reading the sequence configuration file: invalid path ({seq_configuration_path})")
-        '''
